5e36e0e423b9fed16cf46c78380bb0da7629cbf1
Adamkevicius <imatveyadam@gmail.com>
2025-09-22 12:19:40 +0300

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Matvej Adamkevicius on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
