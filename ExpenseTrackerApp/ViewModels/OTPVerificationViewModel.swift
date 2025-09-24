9ed8908e66fa716d8437fc7be30b9b8d7d94d6dd
Adamkevicius <imatveyadam@gmail.com>
2025-09-24 18:57:06 +0300

    OTP implementation

---
+++ ExpenseTrackerApp/ViewModels/OTPVerificationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OTPVerificationViewModel.swift
+//  ExpenseTrackerApp
+//
+//  Created by Matvej Adamkevicius on 22/09/2025.
+//
+
+import Foundation
---
+++ ExpenseTrackerApp/Views/OTPTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  OTPFieldView.swift
+//  ExpenseTrackerApp
+//
+//  Created by Matvej Adamkevicius on 22/09/2025.
+//
+
+import SwiftUI
+
+struct OTPFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OTPFieldView()
+}
